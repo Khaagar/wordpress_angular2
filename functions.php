43123504dc2c1581fbc0fdb309d7d76a9bf18dc9
Marcin Sadowski <marcin.sadowski@contractors.roche.com>
2017-08-24 10:47:00 +0200

    footer layout fixes

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once dirname(__FILE__) . '/inc/custom-routes.php';
 
 require_once dirname(__FILE__) . '/inc/angular-enqueue.php';
@@ -8,7 +7,7 @@ require_once dirname(__FILE__) . '/class-tgm-plugin-activation.php';
 
 class angularjs_wp_theme {
 
-const MENU_STRING = "header-footer-menu";
+    const MENU_STRING = "header-footer-menu";
 
     function init() {
 
@@ -37,9 +36,13 @@ const MENU_STRING = "header-footer-menu";
     /* THEME SUPPORT */
 
     function add_awesome_theme_support() {
-        
+
         $this->register_required_theme_plugins();
+        $this->addDefaultSettings();
         
+        add_image_size( 'frontpage-carousele-size', 1110, 500, array( 'center', 'top' ) );        
+        add_image_size( 'team-carousele-size', 200, 200, array( 'center', 'top' ) );
+
         add_theme_support('post-thumbnails');
         add_post_type_support('page', 'excerpt');
     }
@@ -126,9 +129,7 @@ const MENU_STRING = "header-footer-menu";
             ),
         );
 
-        
-        
-        
+
         /**
          * Array of configuration settings. Amend each line as needed.
          * If you want the default strings to be available under your own theme domain,
@@ -169,8 +170,85 @@ const MENU_STRING = "header-footer-menu";
         tgmpa($plugins, $config);
     }
 
+    public function addDefaultSettings() {
+        $opt = get_site_option('theme_front_page_galery_category_id');
+        if($opt===false){
+            update_site_option('theme_front_page_galery_category_id', 0);
+        }
+        $opt = get_site_option('theme_team_members_galery_category_id');
+        if($opt===false){
+            update_site_option('theme_team_members_galery_category_id', 0);
+        }
+        $opt = get_site_option('theme_team_members_slide_pp_number');
+        if($opt===false){
+            update_site_option('theme_team_members_slide_pp_number', 4);
+        }
+    }
+
 }
 
 $angularJStheme = new angularjs_wp_theme();
 $angularJStheme->init();
-?>
+
+
+add_action('admin_menu', 'theme_custom_settings_update');
+add_action('admin_menu', 'theme_custom_settings_single');
+
+function theme_custom_settings_single() {
+    add_submenu_page(
+            'options-general.php', 'Theme custom settings', 'Theme settings', 'manage_options', 'theme-custom', 'theme_custom_settings_action'
+    );
+}
+
+function theme_custom_settings_update() {
+
+    if (isset($_POST['submit'])) {
+
+        // verify authentication (nonce)
+        if (!isset($_POST['theme_custom_nonce']))
+            return;
+
+        // verify authentication (nonce)
+        if (!wp_verify_nonce($_POST['theme_custom_nonce'], 'theme_custom_nonce'))
+            return;
+        if (isset($_POST['theme_front_page_galery_category_id'])) {
+            update_site_option('theme_front_page_galery_category_id', $_POST['theme_front_page_galery_category_id']);
+        }
+
+        if (isset($_POST['theme_team_members_galery_category_id'])) {
+            update_site_option('theme_team_members_galery_category_id', $_POST['theme_team_members_galery_category_id']);
+        }
+
+        if (isset($_POST['theme_team_members_slide_pp_number'])) {
+            update_site_option('theme_team_members_slide_pp_number', $_POST['theme_team_members_slide_pp_number']);
+        }
+    }
+}
+function theme_custom_settings_action() {
+    ?>
+    <div class="wrap">
+        <h2>Custom Theme settings</h2>
+        <form method="post">
+            <p>
+                <label>
+                    Front page carousele media category Category
+                    <input type="text" value="<?php echo get_site_option('theme_front_page_galery_category_id');?>" name="theme_front_page_galery_category_id" >
+                    <br/>
+                </label>
+                <label>
+                    Team members carousele media category Category
+                    <input type="text" value="<?php echo get_site_option('theme_team_members_galery_category_id');?>" name="theme_team_members_galery_category_id" >
+                    <br/>
+                </label>
+                <label>
+                    Team members per slide number
+                    <input type="text" value="<?php echo get_site_option('theme_team_members_slide_pp_number');?>" name="theme_team_members_slide_pp_number" >
+                    <br/>
+                </label>
+            </p>
+            <?php wp_nonce_field('theme_custom_nonce', 'theme_custom_nonce'); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
